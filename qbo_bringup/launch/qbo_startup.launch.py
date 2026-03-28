fee40a1e88fda7eb64eac36b862650ee1b3a8ae8
FOUCAULT Vincent <elpimous12@gmail.com>
2026-03-28 18:30:53 +0100

    update pour Neo

--- qbo_bringup/launch/qbo_startup.launch.py
+++ qbo_bringup/launch/qbo_startup.launch.py
@@ -1,5 +1,4 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
@@ -9,24 +8,12 @@ from ament_index_python.packages import get_package_share_directory
 # Lancer manuellement le service =>
 
 def generate_launch_description():
-    qbo_driver_dir = get_package_share_directory('qbo_driver')
     qbo_arduqbo_dir = get_package_share_directory('qbo_arduqbo')
 
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(qbo_driver_dir, 'launch', 'qbo_diag.launch.py')
-            )
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
                 os.path.join(qbo_arduqbo_dir, 'launch', 'qbo_full.launch.py')
             )
         )
-        # Node(
-        #     package='qbo_bringup',
-        #     executable='smach.py',
-        #     name='smatch',
-        #     output='screen',
-        # ),
     ])
