f7688765ac31260b5e4a90cdd1fc4ac50c4f896b
FOUCAULT Vincent <elpimous12@gmail.com>
2026-03-28 18:43:01 +0100

    ajout démarrage node camera

--- qbo_bringup/launch/qbo_startup.launch.py
+++ qbo_bringup/launch/qbo_startup.launch.py
@@ -5,15 +5,22 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-# Lancer manuellement le service =>
-
 def generate_launch_description():
     qbo_arduqbo_dir = get_package_share_directory('qbo_arduqbo')
+    qbo_camera_dir = get_package_share_directory('qbo_camera')
 
     return LaunchDescription([
+        # Launch principal du robot
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(qbo_arduqbo_dir, 'launch', 'qbo_full.launch.py')
             )
-        )
+        ),
+
+        # Ajout du launch pour inversion d'image
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(qbo_camera_dir, 'launch', 'invert_image_launch.py')
+            )
+        ),
     ])
